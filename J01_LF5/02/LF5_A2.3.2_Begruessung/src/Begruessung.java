e1bf422c64a56f79770bf989c97d7500b7315d8f
9hax <oszimt-mitschriften@9hax.net>
2024-09-27 09:59:39 +0200

    Add LF5 02 A2.3.2 Begruessung

---
+++ J01_LF5/02/LF5_A2.3.2_Begruessung/src/Begruessung.java
@@ -0,0 +1,8 @@
+public class Begruessung {
+    public static void main(String[] args) {
+        System.out.println("""
+                Hallo Programmierer!
+                Programmieren mit
+                Java macht Spaß.""");
+    }
+}
