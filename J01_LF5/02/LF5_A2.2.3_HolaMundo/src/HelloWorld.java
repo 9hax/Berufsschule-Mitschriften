a18eaf7bd7ed914b04042474db28e2c3848202c2
9hax <oszimt-mitschriften@9hax.net>
2024-09-27 09:25:22 +0200

    Add LF5 02 A2.2.3 Implementation

---
+++ J01_LF5/02/LF5_A2.2.3_HolaMundo/src/HelloWorld.java
@@ -0,0 +1,46 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        // Esto es un comentario!
+
+        /* This program prints the String "Hola Mundo!" to the console output.
+         * This is just a demo of the java programming language.
+         * The text "Hola Mundo!" is the spanish translation of "Hello, World!" as traditionally used in programming.
+         * This is also a multiline comment referencing itself. Pretty meta, eh?
+         */
+        System.out.println("Hola Mundo!");
+
+        /*
+        // 1.
+        System.out.println("Aufgabe 2, 1:")
+
+        System.out.println("Hallo");
+        System.out.println(" Welt!");
+        */
+
+        /*
+        // 2.
+        System.out.println("Aufgabe 2, 2:")
+
+        System.out.print("Hallo");
+        System.out.print(" Welt!");
+        */
+
+        /*
+        // 3.
+        System.out.println("Aufgabe 2, 3:")
+
+        System.out.print("Hallo\n");
+        System.out.print(" Welt!");
+        */
+
+        /*
+        // 4.
+        System.out.println("Aufgabe 2, 4:")
+
+        System.out.print("\tHallo\n");
+        System.out.print(" Welt!");
+        */
+        
+    }
+}
