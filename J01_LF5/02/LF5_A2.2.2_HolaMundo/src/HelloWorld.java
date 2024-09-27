3e387b68447bbb52dcb3a71ea471a3e74a95e541
9hax <oszimt-mitschriften@9hax.net>
2024-09-27 09:12:02 +0200

    Update LF5 02 Add Multiline comment.

--- J01_LF5/02/LF5_A2.2.2_HolaMundo/src/HelloWorld.java
+++ J01_LF5/02/LF5_A2.2.2_HolaMundo/src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
+
     public static void main(String[] args){
+        // Esto es un comentario!
+
+        /* This program prints the String "Hola Mundo!" to the console output.
+         * This is just a demo of the java programming language.
+         * The text "Hola Mundo!" is the spanish translation of "Hello, World!" as traditionally used in programming.
+         * This is also a multiline comment referencing itself. Pretty meta, eh?
+         */
         System.out.println("Hola Mundo!");
     }
 }
