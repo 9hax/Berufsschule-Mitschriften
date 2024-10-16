a95e123da53ef54189dff52ead9ef1b4d2c4f959
9hax <oszimt-mitschriften@9hax.net>
2024-10-16 13:47:30 +0200

    Update LF5 02 remove IntelliJ Run Comment Stub from all projects

--- J01_LF5/03/LF5_A3.4.2_Spielbank/src/Spielbank.java
+++ J01_LF5/03/LF5_A3.4.2_Spielbank/src/Spielbank.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Spielbank {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- J01_LF5/03/LF5_A3.4.4_Wechselstube/src/Wechselstube.java
+++ J01_LF5/03/LF5_A3.4.4_Wechselstube/src/Wechselstube.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Wechselstube {
     public static void main(String[] args) {
         // inialize Scanner
