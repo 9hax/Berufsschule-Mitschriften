7fce2d1e818e3c0ac01aceb40cabc138f001fbc7
9hax <oszimt-mitschriften@9hax.net>
2024-10-18 10:15:23 +0200

    Update LF5 04 A1.1.2 Add Uebung 2

--- J01_LF5/04/LF5_A1.1.2_Uebung1_Fahrkartenautomat/src/A11_Uebung.java
+++ J01_LF5/04/LF5_A1.1.2_Uebung1_Fahrkartenautomat/src/A11_Uebung.java
@@ -2,6 +2,9 @@ public class A11_Uebung {
 
 
     public static void main(String[] args) {
+
+        // Übung 1
+
         byte cent;
         cent = 70;
         cent = 80;
@@ -9,5 +12,14 @@ public class A11_Uebung {
 
         double maximum = 95.50;
         System.out.println(maximum);
+
+        // Übung 2
+
+        boolean uebung2_var1 = true;
+        short uebung2_var2 = -1000;
+        float uebung2_var3 = (float)1.255;
+        char uebung2_var4 = '#';
+        byte uebung2_var5 = 8;
+        long uebung2_var6 = 2147483648L;
     }
 }
