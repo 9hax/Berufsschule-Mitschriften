97649c64cb35c9200dc4b62ff8ef322994c9f744
9hax <oszimt-mitschriften@9hax.net>
2024-10-18 10:20:46 +0200

    Update LF5 04 A1.1.2 Add Uebung 3 and 4

--- J01_LF5/04/LF5_A1.1.2_Uebung1_Fahrkartenautomat/src/A11_Uebung.java
+++ J01_LF5/04/LF5_A1.1.2_Uebung1_Fahrkartenautomat/src/A11_Uebung.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class A11_Uebung {
 
 
@@ -21,5 +23,20 @@ public class A11_Uebung {
         char uebung2_var4 = '#';
         byte uebung2_var5 = 8;
         long uebung2_var6 = 2147483648L;
+
+        // Übung 3
+
+        String uebung3_var1 = "No hablo español.";
+        System.out.println(uebung3_var1);
+
+        final short check_nr = 8755;
+        System.out.println(check_nr);
+
+        /*
+        Übung 4
+
+        Die Verwendung von Datentypen erleichtert dem Compiler und der Runtime das Speichermanagement und Fehlerchecking.
+         */
+
     }
 }
