7a7ac7c530fe2356fbc439c39b1f27ddb4a0612b
9hax <oszimt-mitschriften@9hax.net>
2024-10-18 11:01:53 +0200

    Add LF5 04 A1.1.4 Konfiguration

---
+++ J01_LF5/04/LF5_A1.1.4_Konfogurationsdatei/src/A114_Konfiguration.java
@@ -0,0 +1,41 @@
+public class
+        A114_Konfiguration
+{
+public static void
+main (String[] args)
+        {
+String
+        name =
+        "Automat AVR Q2021_FAB_A";
+char
+                sprachModul =
+'d';
+final
+byte
+PRUEFNR =
+        4;
+        double
+prozent =
+                                49.16;
+                        prozent =
+53.76;
+int
+                euro,
+cent;
+euro =
+        142;
+cent =
+        80;
+boolean
+status =
+        true;
+
+        System.out.println("Name: " + name);
+System.out.println("Sprache: " + sprachModul);
+System.out.println("Prüfnummer : " + PRUEFNR);
+System.out.println("Füllstand: " + prozent + " %");
+System.out.println("Summe Euro: " + euro +  " Euro");
+System.out.println("Summe Rest: " + cent +  " Cent");
+System.out.println("Status: " + status);
+        }
+}
