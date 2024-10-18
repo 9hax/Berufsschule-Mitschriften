a33e30ca34d9baf84c500377e0faad6c0b0783cd
9hax <oszimt-mitschriften@9hax.net>
2024-10-18 10:09:43 +0200

    Add LF5 04 A1.1.2 Uebung 1

---
+++ J01_LF5/04/LF5_A1.1.2_Uebung1_Fahrkartenautomat/src/A11_Uebung.java
@@ -0,0 +1,13 @@
+public class A11_Uebung {
+
+
+    public static void main(String[] args) {
+        byte cent;
+        cent = 70;
+        cent = 80;
+        System.out.println(cent);
+
+        double maximum = 95.50;
+        System.out.println(maximum);
+    }
+}
