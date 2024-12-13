64cedf458af6d5aa09f90b402a3cb18229b2241a
9hax <oszimt-mitschriften@9hax.net>
2024-12-13 09:27:48 +0100

    Add LF5 04 A1.3 Variablentypen

---
+++ J01_LF5/04/LF5_A1.3_Variablentypen/src/A13_WeltDerZahlen.java
@@ -0,0 +1,46 @@
+public class A13_WeltDerZahlen {
+
+    public static void main(String[] args) {
+
+        // Wie viele Einwohner hat Berlin?
+        int bewohnerBerlin = 3432000;
+
+        // Wann wurde der erste Computer gebaut?
+        short ersterComputer = 1956;
+
+        // Wieviele Planeten hat unser Sonnesystem?
+        byte anzahlPlaneten = 9;
+
+        // Wieviele Sonnen sind in der Milchstraße?
+        long sonnenMilchstrasse = 150000000000L;
+
+        // Wie viel wiegt das schwerste Tier der Welt(kg)?
+        int schwerstesTier = 200000;
+
+        // Wie groß ist das kleinste Land der Erde in km2?
+        double flaecheKleinsteLand = 0.44;
+
+        // Wieviel km2 der Erde sind mit Wasser bedeckt?
+        int wasser = 361000000;
+
+        // An wieviel Tagen im Jahr regenet es durchschnittlich in Waialeale auf Hawaii?
+        short regen = 335;
+
+        // Wieviele Menschen leben auf der Erde?
+        long einwohnerErde = 8025000000L;
+
+        // Wie alt bist du? Wie viele Tage sind das?
+        short alterTage = 8243;
+        System.out.println("Bewohner von Berlin: " + bewohnerBerlin);
+        System.out.println("Erste Computer gebaut: " + ersterComputer);
+        System.out.println("Anzahl an Planeten: " + anzahlPlaneten);
+        System.out.println("Sonnen in der Milchstraße: " + sonnenMilchstrasse);
+        System.out.println("Schwerstes Tier: " + schwerstesTier);
+        System.out.println("Fläche kleinste Land (km2): " + flaecheKleinsteLand);
+        System.out.println("Wasser der Erde (km2): " + wasser);
+        System.out.println("Regen in Waialeale (Tagen): " + regen);
+        System.out.println("Einwoher auf der Erde: " + einwohnerErde);
+        System.out.println("Alter in  Tagen: " + alterTage);
+
+    }
+}
