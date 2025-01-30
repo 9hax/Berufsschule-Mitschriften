d8be3a1c2af72b40d899695802926681ae508bc5
Lydia Vierkorn <me@9hax.net>
2025-01-30 01:01:55 +0100

    Update LF5 05 Twist: Remove Breadcrumbs.

--- J01_LF5/05/Zusatzaufgabe_Twist/src/DataLoaderInterface.java
+++
@@ -1,8 +0,0 @@
-import java.nio.file.Path;
-import java.util.ArrayList;
-
-public interface DataLoaderInterface {
-    ArrayList<String> loadData(Path sourceFile);
-    // returns null on errors and a list of words on successful load.
-}
-
